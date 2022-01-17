2bdd59b590f61f8a0f962e962fc3666482def91f
Mathis POL <mathis.pol@etudiant.univ-reims.fr>
2022-01-17 09:29:05 +0100

    Ajout d'un ecouteur d'evenement pour mettre en cache les ressources necessaires

---
+++ sw.js
@@ -0,0 +1,31 @@
+const STATIC_CACHE_NAME = "todosApp.v0"
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(STATIC_CACHE_NAME).then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/icon/icon-512x512.png',
+                '/js/apiRequest.js',
+                '/js/controller.js',
+                '/js/ihm.js',
+                '/manifest.json',
+                '/sw.js',
+                '/icon/apple-icon-120x120-seochecker-manifest-5510.png',
+                '/icon/android-icon-192x192-seochecker-manifest-5510.png',
+                '/icon/apple-icon-57x57-seochecker-manifest-5510.png',
+                '/icon/apple-icon-72x72-seochecker-manifest-5510.png',
+                '/icon/apple-icon-114x114-seochecker-manifest-5510.png',
+                '/icon/apple-icon-76x76-seochecker-manifest-5510.png',
+                '/icon/apple-icon-60x60-seochecker-manifest-5510.png',
+                '/icon/favicon-96x96-seochecker-manifest-5510.png',
+                '/icon/favicon-32x32-seochecker-manifest-5510.png',
+                '/icon/favicon-16x16-seochecker-manifest-5510.png',
+                '/icon/apple-icon-152x152-seochecker-manifest-5510.png',
+                '/icon/apple-icon-144x144-seochecker-manifest-5510.png',
+                '/icon/apple-icon-180x180-seochecker-manifest-5510.png'
+            ]);
+        })
+    );
+});
