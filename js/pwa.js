5afb9dfd0930f2c874ab379249240c0503d6bf4b
Mathis POL <mathis.pol@etudiant.univ-reims.fr>
2022-01-24 10:33:25 +0100

    boutton d'installation de l'application fonctionelle

---
+++ js/pwa.js
@@ -0,0 +1,40 @@
+const conteneur_boutton = document.getElementById("button_install");
+const buttonInstall = document.getElementById("button");
+
+console.log(conteneur_boutton, buttonInstall);
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+function showInstallPromotion(){
+    conteneur_boutton.classList.remove('hidden');
+}
+
+function hideInstallPromotion(){
+    conteneur_boutton.classList.add('hidden');
+}
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    console.log('beforeinstallprompt', e);
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
+})
+
+buttonInstall.addEventListener('click', async () => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+});
